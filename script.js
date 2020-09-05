a6083d833470243bb4b119a1c0748d1572ead051
MamzelNyf <fanny@shedesign.dev>
2020-09-05 11:54:19 +0800

    exercice 1

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
-// window.onload = function() {
-   
-// }
+//Exercice 1
+function changeNumber(n){
+let number = parseInt(document.getElementById('number').value,10);
+isNaN(number) ?  number = "Enter a number" : number += n; 
+document.getElementById('number').value = number
+}
 
+function decrementValue(){
+   changeNumber(-1)
+}
+function incrementValue(){
+   changeNumber(1)
+}
 
-// document.addEventListener('keydown', event => {
-//    [nameFunction](event.key)
-// });
+//Exercice 2
+window.onload = () => {
+   const buttons = document.getElementById('items').querySelectorAll('button')
+   buttons.forEach(button => {
+      button.addEventListener('click', addToCart)
+   })
+}
 
+function addToCart(e){
+   const button = e.currentTarget
+   const paragraph = button.parentElement.children[0]
+   console.log(paragraph.innerText)
+   var li = document.createElement("li");
+   var t = document.createTextNode(paragraph.innerText);
+   li.appendChild(t);
+   document.getElementById("cart").appendChild(li);
+
+}
 
 
